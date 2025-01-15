6e38bf3f25610845a20f7b443b79a5e46e542f19
Danny Vivas <dvivasmera1.2@gmail.com>
2025-01-15 00:21:35 -0500

    first commit

--- src/TaskCard.jsx
+++ src/TaskCard.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { TaskContext } from './context/TaskContext';
 
-export default function Taskcard({ task = [] }) {
+export default function TaskCard({ task = [] }) {
   const { deleteTask } = useContext(TaskContext);
   function handleDeleteTask() {
     deleteTask(task.id);
